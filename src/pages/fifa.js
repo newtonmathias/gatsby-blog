9c565c1d67ba44c630cb9e81736fae8165caa6d7
Newton Mathias <newtymathias@gmail.com>
2019-08-28 14:34:51 +0300

    sdfghhj

--- src/pages/fifa.js
+++ src/pages/fifa.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Layout from "../components/layout"
+import { Link } from 'gatsby'
+
 
 
 const fifa = () => {
   return (
     <Layout>
-      Todays fifa
+      <h3>Welcome to the Fifa section</h3>
+      <p>
+      This will be all about Fifa20 tips and tricks to help you improve your gaming.
+        we will also take a look at all things happening in the FIFA community. We will also
+        review gameplay, patches and new features in the game.
+        In the meantime, head over to the <Link to="/news">News</Link> section for top stories
+      </p>
     </Layout>
   )
 }
--- src/pages/matchcenter.js
+++ src/pages/matchcenter.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Layout from "../components/layout"
+import { Link } from 'gatsby'
+
 
 
 const MatchCenter = () => {
   return (
     <Layout>
-      Todays MatchCenter
+      <h3>Welcome to the Match Center section</h3>
+      <p>This section will feature match reviews and analysis. 
+       It will also take a look into upcoming matches and stats.
+       we will also review players and any other relevant stories.
+       In the meantime, head over to the <Link to="/news">News</Link> section for top stories
+      </p>
     </Layout>
   )
 }
